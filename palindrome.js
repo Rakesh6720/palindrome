306bc4c39d5a11cff04ac81819f32e3d12664c79
Rakesh <rbaruah@asusLaptop.(none)>
2018-04-09 12:11:04 -0500

    first commit

---
+++ palindrome.js
@@ -0,0 +1,35 @@
+
+// Step 1: prompt the user to enter a string
+let userString = prompt("Please enter a single word:");
+// Step 2: set  new variable to result of function, call function
+let backwardString = reverseString(userString);
+// Step 3: compare the user's string with it's palindrome.  
+if (backwardString === userString) {
+		console.log(backwardString + " and " + userString + " are palindromes!");
+	}
+	else {
+		console.log(backwardString + " and " + userString + " are not palindromes!");
+	}
+
+
+function reverseString(str) {
+	//split the string into individual elements of an array
+	let stringArray = str.split("");
+	// stringArray = ["s", "t", "r"];
+	//reverse the string array into a new array
+	let reverseArray = stringArray.reverse();
+	// reverseArray = ["r", "t", "s"];
+	// join the new array into a new string
+	return reverseArray.join("");
+}
+
+/*
+	// backwardString = "rts"
+	//compare the new string to the old sring, if they are the same then it is a palindrome, if not, fuck it;
+	if backwardString === userString {
+		console.log(backwardString + " and " + userString + " are palindromes!");
+	}
+	else {
+		console.log(backwardString + " and " + userString + " are not palindromes!");
+	}
+*/
